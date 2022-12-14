08db218a37d4189c7d8a7777ccd22c63169bb4f5
22-BrandonGuerrero <brandon.guerrero@vallegrande.edu.pe>
2022-12-14 13:23:20 -0500

    a

--- registro.js
+++ registro.js
@@ -1,5 +1,4 @@
-const url = "http://localhost:3000/api/pedido/";
-let resultados = '';
+
 const formArticulo = document.querySelector("form");
 const userped = document.getElementById("USERPED");
 const emausped = document.getElementById("EMAUSPED");
